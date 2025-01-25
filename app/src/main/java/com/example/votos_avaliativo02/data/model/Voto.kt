32710961429a184ef22b2f6fcf34ffa1bc7c1bf1
gabrieldepauli <gabrieldepauli.ads@gmail.com>
2025-01-24 21:04:17 -0300

    Adicionando método de configurar String de Voto

--- app/src/main/java/com/example/votos_avaliativo02/data/model/Voto.kt
+++ app/src/main/java/com/example/votos_avaliativo02/data/model/Voto.kt
@@ -1,4 +1,13 @@
 package com.example.votos_avaliativo02.data.model
 
 class Voto( val codigo: String, val valor: Int) {
+
+    // Método criado para gerar o código de Voto
+    companion object {
+        fun generateRandomCode(): String {
+            val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            return (1..10).map { chars.random() }.joinToString("")
+        }
+    }
+
 }
